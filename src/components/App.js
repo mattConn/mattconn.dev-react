8e960445f2b6e1b2d2bb780e2cb5de20d929ee01
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-06-15 18:13:01 -0400

    added resume component

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,18 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import About from '../pages/About';
+import Resume from '../pages/Resume';
 import NotFound from '../pages/NotFound';
 
 export default class App extends Component{
     navLinks = {
-        Home: "/",
-        Resume: "storage/resume-2019.pdf",
-        Portfolio: "portfolio.html",
-        GitHub: "https://github.com/mattConn/",
-        LinkedIn: "https://www.linkedin.com/in/mattconndev/",
-        Twitter: "https://twitter.com/mattconndev",
-        Blog: "https://medium.com/@mattconndev"
+        Home: '/',
+        Resume: '/resume',
+        Portfolio: 'portfolio',
+        GitHub: 'https://github.com/mattConn/',
+        LinkedIn: 'https://www.linkedin.com/in/mattconndev/',
+        Twitter: 'https://twitter.com/mattconndev',
+        Blog: 'https://medium.com/@mattconndev'
     };
 
     render(){
@@ -24,6 +25,7 @@ export default class App extends Component{
                     <Header navLinks = {this.navLinks} />
                         <Switch>
                             <Route path="/" exact component={About} />
+                            <Route path="/resume" component={Resume} />
                             <Route component={NotFound} />
                         </Switch>
                 </BrowserRouter>
