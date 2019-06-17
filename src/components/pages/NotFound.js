e05175ec208dc83cd0c4a8bf009ce67ec16ef7a0
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-06-17 16:26:13 -0400

    edited notFound page title

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Page from './Page';
 
 export default class NotFound extends Page {
-    title = `${location.pathname.replace('/', '')} Not Found`;
+    expectedTitle = location.pathname.replace('/', '');
+    title = `${this.expectedTitle} Not Found`;
 
     render() {
         return (
             <div>
                 <h1>Page Not Found.</h1>
-                <h3>Could not find <em>{this.title}</em>. I'm sorry!</h3>
+                <h3>Could not find <em>{this.expectedTitle}</em>. I'm sorry!</h3>
             </div>
         );
     }
